1878c7274e1787bd49e159b62269b3cdace66275
rigmaumesh@gmail.com <rigmaumesh34@gmail.com>
2026-05-15 20:08:29 +0530

    Create tasks table migration with fields for title, description, status, due date, assigned user, AI summary, and priority

---
+++ database/migrations/2026_05_15_143152_create_tasks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', [
+                        'pending',
+                        'in_progress',
+                        'completed'])
+                    ->default('pending');
+            $table->date('due_date');
+            $table->foreignId('assigned_to')
+                    ->constrained('users')
+                    ->cascadeOnDelete();
+            $table->text('ai_summary')->nullable();
+            $table->enum('ai_priority', [
+                'low',
+                'medium',
+                'high'
+            ])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
